858200e8d3ea99de1b7149211e5a569cb5197cd8
Akshay Tekam <tekamakshay11@gmail.com>
2020-11-16 11:19:26 +0530

    Update Emp_Wage_UC6.java

--- Emp_Wage_UC6.java
+++ Emp_Wage_UC6.java
@@ -2,40 +2,38 @@
 package dayninteen;
 
  
-public class Emp_Wage_UC6
-{
+public class Emp_Wage_UC6{
+	
 public static final int IS_PART_TIME = 1;
 public static final int IS_FULL_TIME = 2;
 public static final int EMP_RATE_PER_HOUR = 20;
 public static final int NUM_OF_WORKING_DAYS = 2;
 public static final int MAX_HRS_IN_MONTH = 10;
 
-public static void main(String[] args)
-{
+public static void main(String[] args){
+	
 // Variables
 
-int totalEmpWage =0;
-int totalWorkingDays =0;
-int totalEmpHrs =0;
+int totalEmpWage =0, totalWorkingDays =0, totalEmpHrs =0;
 	//Computation
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-{
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+	
 int empHrs = 0;
 totalWorkingDays++;
 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-switch (empCheck)
-{
-case IS_PART_TIME:
+switch (empCheck){
+		
+ case IS_PART_TIME:
 	empHrs =4;
 	break;
-case IS_FULL_TIME:
+ case IS_FULL_TIME:
 	empHrs =8;
 	break;
-default:
+ default:
 	empHrs =0;
 }
-totalEmpHrs += empHrs;
-System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
+  totalEmpHrs += empHrs;
+  System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
 } 
 
 totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
